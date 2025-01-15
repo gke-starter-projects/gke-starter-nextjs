67140e8c83629885d7b06e7dabcd9b0dbf303f70
Souradeep Nanda <souradeep.nanda123@gmail.com>
2025-01-15 11:50:19 -0600

    lint fix

--- src/components/Element.js
+++ src/components/Element.js
@@ -2,35 +2,32 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 // Base Element component that renders a single element
-const Element = ({ 
+function Element({
   as = 'span',
   children,
   className = '',
-  ...props 
-}) => {
+  ...props
+}) {
   return React.createElement(
     as,
     {
       className,
-      ...props
+      ...props,
     },
-    children
+    children,
   );
-};
+}
 
 Element.propTypes = {
   as: PropTypes.string,
   children: PropTypes.node,
   className: PropTypes.string,
-  // Allow any other props
-   
-  [PropTypes.string]: PropTypes.any
 };
 
 Element.defaultProps = {
   as: 'span',
   children: null,
-  className: ''
+  className: '',
 };
 
 export default Element;
